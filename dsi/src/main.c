985a7b040efae680082c6e802e11af033255aef1
Amy Parker <amy@amyip.net>
2024-09-25 20:13:49 -0700

    working wireless
    Signed-off-by: Amy Parker <amy@amyip.net>

--- dsi/src/main.c
+++ dsi/src/main.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#include <netinet/in.h>
+
 #include <nds.h>
+#include <dswifi9.h>
 
 volatile int frame = 0;
 
@@ -14,12 +17,21 @@ void Vblank() {
 int main() {
     // Declare common variables
     touchPosition touchXY;
+    struct in_addr ip, gateway, inil;
 
     // Initialize irq handlers
     irqSet(IRQ_VBLANK, Vblank);
 
     consoleDemoInit();
-    iprintf("Compiled with devkitNix");
+    iprintf("Compiled with devkitNix\n");
+    iprintf("Connecting via WFC data...\n");
+    if (!Wifi_InitDefault(WFC_CONNECT)) {
+        iprintf("Connection failed.");
+    } else {
+        iprintf("Connected! ");
+        ip = Wifi_GetIPInfo(&gateway, &inil, &inil, &inil);
+        iprintf("ip=%s,gateway=%s", inet_ntoa(ip), inet_ntoa(gateway));
+    }
 
     while (true) {
         // Avoid running like crazy through DSi power by waiting for the next frame
@@ -29,8 +41,8 @@ int main() {
         if (keysDown() & KEY_START)
             break;
         touchRead(&touchXY);
-        iprintf("\x1b[10;0HFrame = %d", frame);
-        iprintf("\x1b[16;0HTouch x = %04X, %04X\n", touchXY.rawx, touchXY.px);
+        iprintf("\x1b[12;0HFrame = %d", frame);
+        iprintf("\x1b[14;0HTouch x = %04X, %04X\n", touchXY.rawx, touchXY.px);
         iprintf("Touch y = %04X, %04X\n", touchXY.rawy, touchXY.py);
     }
     return 0;
